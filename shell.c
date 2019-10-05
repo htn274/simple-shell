5b581b75008f8f74b74c1782d7e590d6e3a9015d
Hy Vuong <vuonghy2442@hotmail.com.vn>
2019-10-05 14:39:56 +0700

    temp

--- shell.c
+++ shell.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/types.h>
 #include <fcntl.h>
 #include <readline/readline.h>
+
 
 #define MAXLINE 80
 #define MAXARGS 40
 
-#define IN 0
-#define OUT 1
-
+#define RIN_FLAG 1
+#define ROUT_FLAG 2
 
 int parseCmd(char* input, char* arg[])
 {
     int i;
-    for (i = 0; i < MAXARGS; i++)
-    {
+    for (i = 0; i < MAXARGS; i++) {
         arg[i] = strsep(&input, " ");
         if (arg[i] == NULL) 
             break;
+
         if (strlen(arg[i]) == 0)
             i--;
     }
@@ -28,183 +31,102 @@ int parseCmd(char* input, char* arg[])
 void addHistory(char *arg[], int len, char *his[])
 {
     int i = 0;
-    char *str;
-    while (i < len)
-    {
+    //char *str;
+    while (i < len) {
         his[i] = (char*)malloc((strlen(arg[i]) + 1) * sizeof(char));
         strcpy(his[i], arg[i]);
         i++;
     }
-    for (int j = len; j < MAXARGS; j++)
-    {
+
+    for (int j = len; j < MAXARGS; j++) {
         if (his[j] == NULL)
             break;
         his[j] = NULL;
     }
 }
 
-void execCmd(char* arg[])
+pid_t fexecCmd(char **args, const int fd[2])
 {
-    //Create a child process
-    pid_t pid = fork();
-    if (pid == -1)
-    {
-        perror("Error");
-        return;
-    }
-    if (pid == 0)
-    {
-        if (execvp(arg[0], arg) < 0)
-        {
-            perror("Command error");
+    pid_t p = vfork();
+    if (p < 0)
+        return -1;
+
+    if (p == 0) {
+        if (fd[0] >= 0) {
+            if (dup2(fd[0], STDIN_FILENO) < 0)
+                exit(1);
         }
-        exit(1);
-    }
-    else
-    {
-        wait(NULL);
-        return;
+
+        if (fd[1] >= 0) {
+            if (dup2(fd[1], STDOUT_FILENO) < 0)
+                exit(1);           
+        }
+
+        close(fd[0]);
+        close(fd[1]);
+        execvp(args[0], args);
+        exit(1); //error
+    } else {
+        return p;
     }
 }
 
-void execPipe(char* pipeWriter[], char* pipeReader[])
+void execCmd(char **args) {
+    const int fd[2] = {-1, -1};
+    waitpid(fexecCmd(args, fd), NULL, 0);    
+}
+
+void execPipe(char **pipeWriter, char **pipeReader)
 {
     // 0: read, 1: write
     int fd[2];
-    pid_t p1, p2;
 
     if (pipe(fd) < 0)
-    {
-        perror("Initialized pipe error");
         return;
-    }
 
-    p1 = fork();
+    int fd0[2] = { fd[0], -1};
+    int fd1[2] = { -1, fd[1]};
 
-    if (p1 < 0)
-    {
-        perror("Pipe error");
-        return;
-    }
+    pid_t p1 = fexecCmd(pipeWriter,  fd1);
+    pid_t p2 = fexecCmd(pipeReader,  fd0);
 
-    if (p1 == 0)
-    {
-        // p1 write the result to the pipe
-        //close(fd[0]);
-        fflush(stdout);
-        if (dup2(fd[1], STDOUT_FILENO) < 0)
-        {
-            perror("Pipe error");
-            exit(1);
-        }
-        close(fd[1]);
-        close(fd[0]);
+    close(fd[0]);
+    close(fd[1]);
 
-       if (execvp(pipeWriter[0], pipeWriter) < 0)
-       {
-           perror("Pipe error");
-           exit(1);
-       }
-    }
-    else
-    {
-        // run p2
-        p2 = fork();
-
-        if (p2 < 0)
-        {
-            perror("Pipe error");
-            return;
-        }
-
-        if (p2 == 0)
-        {
-            //close(fd[1]);
-            dup2(fd[0], STDIN_FILENO);
-            close(fd[0]);
-            close(fd[1]);
-
-            if (execvp(pipeReader[0], pipeReader) < 0)
-            {
-                perror("Pipe error");
-                exit(1);
-            }
-        }
-        else
-        {
-            close(fd[0]);
-            close(fd[1]);
-            wait(NULL);
-            wait(NULL);
-        }
-    }
+    waitpid(p1, NULL, 0);
+    waitpid(p2, NULL, 0);
 }
 
 
-void execRedirection(char *arg[], char* filename, int type)
+void execRedir(char **args, const char * const filename[2], int type)
 {
-    int fd;
-    pid_t pid;
-    pid = fork();
+    int fd[2] = {-1, -1};
     mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
-    int tmpIN = dup(IN), tmpOUT = dup(OUT);
 
-    if (type == IN)
-        {
-            fd = open(filename, O_RDONLY, mode);
-            if (fd < 0)
-            {
-                perror("Redirect Input Error");
-                exit(1);
-            }
-            if (dup2(fd, STDIN_FILENO) < 0)
-            {
-                perror("Redirect Input Error");
-                exit(1);
-            }
-            close(fd);
-        }
-        else
-        {
-            fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, mode);
-            if (fd < 0)
-            {
-                perror("Redirect Output Error");
-                exit(1);
-            }
-
-            if (dup2(fd, STDOUT_FILENO) < 0)
-            {
-                perror("Redirect Output Error");
-                exit(1);
-            }
-            close(fd);
-        }
-    if (pid == -1)
+    if (type & RIN_FLAG)
     {
-        perror("Redirect Error");
-        exit(1);
+        fd[0] = open(filename[0], O_RDONLY, mode);
+        if (fd[0] < 0)
+        {
+            perror("Redirect Input Error");
+            return;
+        }
+    }
+    
+    if (type & ROUT_FLAG)
+    {
+        fd[1] = open(filename[1], O_WRONLY | O_CREAT, mode);
+        if (fd[1] < 0)
+        {
+            perror("Redirect Output Error");
+            return;
+        }
     }
 
-    if (pid == 0)
-    {
-        if (execvp(arg[0], arg) < 0)
-        {
-            perror("Redirect Error");
-            exit(1);
-        }
-        close(fd);
-    }
-    else
-    {
-        close(fd);
-        dup2(tmpIN, IN);
-        dup2(tmpOUT, OUT);
-        close(tmpIN);
-        close(tmpOUT);
-        wait(NULL);
-        return;
-    }
+    waitpid(fexecCmd(args, fd), NULL, 0);
+
+    close(fd[0]);
+    close(fd[1]);
 }
 
 
@@ -213,7 +135,7 @@ void printTest(char* arg[])
     int i = 0;
     while (arg[i] != NULL && i < MAXARGS)
     {
-        printf("%s ", arg[i]);
+        printf("%s<space>", arg[i]);
         i++;
     }
     printf("\n");
@@ -223,12 +145,12 @@ void testPipe()
 {
     char *first, *second;
     first = readline("first: ");
-    fflush(stdin);
+    fsync(STDOUT_FILENO);
     second = readline("second: ");
     char *arg1[MAXARGS], *arg2[MAXARGS];
 
-    int num = parseCmd(first, arg1);
-    num = parseCmd(second, arg2);
+    parseCmd(first, arg1);
+    parseCmd(second, arg2);
 
     execPipe(arg1, arg2);
     printf("Test pipe: sucessful\n");
@@ -236,15 +158,17 @@ void testPipe()
 
 void testRedirect()
 {
-    char *cmd, *filename;
+    char *cmd, *filename[2] = {NULL, NULL};
     int type;
     cmd = readline("redir: ");
-    filename = readline("file name: ");
     printf("type redirect: ");
     scanf("%d", &type);
-    char* arg[MAXARGS];
-    parseCmd(cmd, arg);
-    execRedirection(arg, filename, type);
+    filename[type] = readline("file name: ");
+
+    char* args[MAXARGS];
+    parseCmd(cmd, args);
+    printTest(args);
+    execRedir(args, filename, type + 1);
     printf("Test Redirection: sucessful\n");
 }
 
@@ -257,8 +181,8 @@ int main()
     
     while (1)
     {
-        //testPipe();
-        testRedirect();
+        testPipe();
+        //testRedirect();
     }
       
     /*
