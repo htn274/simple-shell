718407b0f0c542427edbd46f8cd4085f48fe1ab9
Hy Vuong <vuonghy2442@hotmail.com.vn>
2019-10-05 18:05:46 +0700

    almost done :3

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
 all: shell.c
-	gcc -Wall -o shell shell.c -lreadline && ./shell
+	gcc -Wall -O2 -o shell shell.c -lreadline
+
+debug:
+	gcc -Wall -g -o shell shell.c -lreadline
+
+debug2:
+	gcc -Wall -g -o shell shell.c -lreadline -fsanitize=address,undefined
 
 clean:
 	rm shell
--- shell.c
+++ shell.c
@@ -6,44 +6,30 @@
 #include <sys/types.h>
 #include <fcntl.h>
 #include <readline/readline.h>
+#include <readline/history.h>
+#include <signal.h>
 
+#define SHELL_NAME "\x1B[38;5;2m\x1B[1mosh>\x1B[0m "
 
-#define MAXLINE 80
-#define MAXARGS 40
+int running = 0;
 
-#define RIN_FLAG 1
-#define ROUT_FLAG 2
-
-int parseCmd(char* input, char* arg[])
+void handler(int sig)
 {
-    int i;
-    for (i = 0; i < MAXARGS; i++) {
-        arg[i] = strsep(&input, " ");
-        if (arg[i] == NULL) 
-            break;
-
-        if (strlen(arg[i]) == 0)
-            i--;
-    }
-    return i;
+    fputs("\n", stdout);
+    if (!running)
+        fputs(SHELL_NAME, stdout);
+        
 }
 
-void addHistory(char *arg[], int len, char *his[])
-{
-    int i = 0;
-    //char *str;
-    while (i < len) {
-        his[i] = (char*)malloc((strlen(arg[i]) + 1) * sizeof(char));
-        strcpy(his[i], arg[i]);
-        i++;
-    }
+struct command {
+    char **args;
+    int async;
 
-    for (int j = len; j < MAXARGS; j++) {
-        if (his[j] == NULL)
-            break;
-        his[j] = NULL;
-    }
-}
+    char *filename[2];
+    int pipe; //true mean pipe, when pipe to next command
+
+    struct command *next;
+};
 
 pid_t fexecCmd(char **args, const int fd[2])
 {
@@ -53,163 +39,283 @@ pid_t fexecCmd(char **args, const int fd[2])
 
     if (p == 0) {
         if (fd[0] >= 0) {
-            if (dup2(fd[0], STDIN_FILENO) < 0)
+            if (dup2(fd[0], STDIN_FILENO) < 0) {
+                perror("Redirect in fail");
                 exit(1);
+            }
+            close(fd[0]);
         }
 
         if (fd[1] >= 0) {
-            if (dup2(fd[1], STDOUT_FILENO) < 0)
-                exit(1);           
+            if (dup2(fd[1], STDOUT_FILENO) < 0) {
+                perror("Redirect out fail");
+                exit(1);     
+            }
+            close(fd[1]);
         }
-
-        close(fd[0]);
-        close(fd[1]);
+    
         execvp(args[0], args);
+        perror("Exec cmd fail");
         exit(1); //error
     } else {
+        if (fd[0] >= 0)
+            close(fd[0]);
+        
+        if (fd[1] >= 0)
+            close(fd[1]);
+
         return p;
     }
 }
 
-void execCmd(char **args) {
-    const int fd[2] = {-1, -1};
-    waitpid(fexecCmd(args, fd), NULL, 0);    
+int redir_in(int fd[2], char *file) {
+    if (fd[0] >= 0) {
+        fputs("Can't have two input\n", stderr);
+        return -1;
+    }
+
+    fd[0] = open(file, O_RDONLY, 0);
+    if (fd[0] < 0)
+    {
+        perror("Redirect Input Error");
+        return -1;
+    }
+
+    return 0;
 }
 
-void execPipe(char **pipeWriter, char **pipeReader)
-{
-    // 0: read, 1: write
-    int fd[2];
+int redir_out(int fd[2], char *file) {
+    const mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+    if (fd[1] >= 0) {
+        fputs("Can't have two output\n", stderr);
+        return -1;
+    }
 
-    if (pipe(fd) < 0)
+    fd[1] = open(file, O_WRONLY | O_CREAT, mode);
+    if (fd[1] < 0)
+    {
+        perror("Redirect Output Error");
+        return -1;
+    }
+    return 0;
+}
+
+int pipe_next(int cfd[2], int nfd[2]) {
+    if (cfd[1] >= 0) {
+        fputs("Can't have two output\n", stderr);
+        return -1;
+    }
+
+    if (nfd[0] >= 0) {
+        fputs("Can't have two input\n", stderr);
+        return -1;
+    }
+
+    int pfd[2];
+    if (pipe(pfd) < 0) {
+        perror("Pipe error");
+        return -1;
+    }
+
+    cfd[1] = pfd[1];
+    nfd[0] = pfd[0]; 
+    return 0;
+}
+
+void move_fd(int cfd[2], int nfd[2]) {
+    cfd[0] = nfd[0];
+    cfd[1] = nfd[1];
+
+    nfd[0] = nfd[1] = -1;
+}
+
+
+int run_command(struct command *cmd) {
+    running = 1;
+
+    int cfd[2] = {-1, -1};
+    int nfd[2] = {-1, -1};
+
+    struct command *p = cmd;
+
+    while (p) {
+        if (p->filename[0])
+            redir_in(cfd, p->filename[0]);
+
+        if (p->filename[1])
+            redir_out(cfd, p->filename[1]);
+
+        if (p->pipe)
+            pipe_next(cfd, nfd);
+
+        pid_t pid = fexecCmd(p->args, cfd);
+
+        if (!p->async) {
+            waitpid(pid, NULL, 0);
+        }
+
+        move_fd(cfd, nfd);
+        p = p->next;
+    }
+
+    running = 0;
+
+    return 0;
+}
+
+void skip_space(char *s, int *x) {
+    int i = *x;
+
+    while (s[i] && isspace(s[i]))
+        ++i;
+
+    *x = i;
+}
+
+int split(char* s, int *x)
+{
+    int i = *x;
+    int j = i;
+
+    char delim = ' ';
+
+    int plain = 1;
+
+    for (;s[i]; ++i) {
+        if (delim != ' ') {
+            if (s[i] == delim) 
+                delim = ' ';
+            else
+                s[j++] = s[i];
+            
+        } else {
+            if (isspace(s[i])) {
+                s[j++] = '\0';
+                ++i;
+                break;
+            } else if (s[i] == '"' || s[i] == '\'') {
+                //meo bad for operator
+                plain = 0;
+                delim = s[i];
+            } else {
+                s[j++] = s[i];
+            }
+        }
+    };
+
+    if (!s[i])
+        s[j++] = '\0';
+
+    *x = i;
+
+    skip_space(s, x);
+
+    return plain;
+}
+
+void free_command(struct command *c) {
+    if (!c)
         return;
 
-    int fd0[2] = { fd[0], -1};
-    int fd1[2] = { -1, fd[1]};
-
-    pid_t p1 = fexecCmd(pipeWriter,  fd1);
-    pid_t p2 = fexecCmd(pipeReader,  fd0);
-
-    close(fd[0]);
-    close(fd[1]);
-
-    waitpid(p1, NULL, 0);
-    waitpid(p2, NULL, 0);
+    free(c->args);
+    free_command(c->next);
+    free(c);
 }
 
+struct command *parseCmd(char *cmd) {
 
-void execRedir(char **args, const char * const filename[2], int type)
-{
-    int fd[2] = {-1, -1};
-    mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
-
-    if (type & RIN_FLAG)
-    {
-        fd[0] = open(filename[0], O_RDONLY, mode);
-        if (fd[0] < 0)
-        {
-            perror("Redirect Input Error");
-            return;
-        }
-    }
-    
-    if (type & ROUT_FLAG)
-    {
-        fd[1] = open(filename[1], O_WRONLY | O_CREAT, mode);
-        if (fd[1] < 0)
-        {
-            perror("Redirect Output Error");
-            return;
-        }
-    }
-
-    waitpid(fexecCmd(args, fd), NULL, 0);
-
-    close(fd[0]);
-    close(fd[1]);
-}
-
-
-void printTest(char* arg[])
-{
     int i = 0;
-    while (arg[i] != NULL && i < MAXARGS)
-    {
-        printf("%s<space>", arg[i]);
-        i++;
-    }
-    printf("\n");
-}
+    int pos = 0;
+    int argc = 0;
 
-void testPipe()
-{
-    char *first, *second;
-    first = readline("first: ");
-    fsync(STDOUT_FILENO);
-    second = readline("second: ");
-    char *arg1[MAXARGS], *arg2[MAXARGS];
+    skip_space(cmd, &pos);
 
-    parseCmd(first, arg1);
-    parseCmd(second, arg2);
+    if (cmd[pos] == '\0')
+        return NULL;
 
-    execPipe(arg1, arg2);
-    printf("Test pipe: sucessful\n");
-}
+    struct command *c = calloc(1, sizeof(struct command));
+    struct command *head = c;
+    struct command *prev = NULL;
 
-void testRedirect()
-{
-    char *cmd, *filename[2] = {NULL, NULL};
-    int type;
-    cmd = readline("redir: ");
-    printf("type redirect: ");
-    scanf("%d", &type);
-    filename[type] = readline("file name: ");
+    do {
+        i = pos;
 
-    char* args[MAXARGS];
-    parseCmd(cmd, args);
-    printTest(args);
-    execRedir(args, filename, type + 1);
-    printf("Test Redirection: sucessful\n");
+        int plain = split(cmd, &pos);
+
+        int is_arg = !plain;
+        int is_done = 0;
+
+        if (plain) {
+            if (cmd[i] == '\0') {
+                is_arg = 0;
+                is_done = 1;
+            } else if (strcmp(cmd + i, "|") == 0) {
+                c->pipe = 1;
+                is_done = 1;
+            } else if (strcmp(cmd + i, "&&") == 0) {
+                is_done = 1;
+            } else if (strcmp(cmd + i, "&") == 0) {
+                c->async = 1;
+            } else if (strcmp(cmd + i, "<") == 0) {
+                int j = pos;
+                split(cmd, &pos); //this is file :P
+                c->filename[0] = cmd + j;
+            } else if (strcmp(cmd + i, ">") == 0) {
+                int j = pos;
+                split(cmd, &pos); //this is file :P
+                c->filename[1] = cmd + j;
+            } else {
+                is_arg = 1;
+            }
+        }
+
+        if (is_arg){
+            ++argc;
+            c->args = realloc(c->args, sizeof(char*) * (argc + 1));
+            c->args[argc - 1] = cmd + i;
+        }
+
+        if (is_done) {
+            if (!c->args) {
+                fputs("Command with no arguments\n", stderr);
+                //free here
+                free_command(head);
+                return NULL;
+            }
+
+            c->args[argc] = NULL;
+            argc = 0;
+            //add to head
+            c->next = calloc(1, sizeof(struct command));
+            prev = c;
+            c = c->next;
+        }
+    } while(cmd[i] != '\0');
+
+    free(c);
+    prev->next = NULL;
+    
+    return head;
 }
 
 int main()
 {
-    char *arg[MAXARGS], *history[MAXARGS];
-    char *input;
-    int numberArg;
-
+    signal(SIGINT,handler);
     
+    char *s;
     while (1)
     {
-        testPipe();
-        //testRedirect();
+        s = readline(SHELL_NAME);
+        if (!s)
+            break;
+
+        add_history(s);
+        
+        struct command *c = parseCmd(s);
+        run_command(c);
+        free_command(c);
+        free(s);
     }
-      
-    /*
-    while (1){
-        input = readline("osh> "); 
-        numberArg = parseCmd(input, arg);
-        //printTest(arg);
-        if (strcmp(arg[0], "!!") == 0)
-        {
-            if (history[0] == NULL)
-            {
-                printf("No history command!\n");
-            }
-            else
-            {
-                printf("osh> ");
-                printTest(history);
-                execCmd(history);
-            }
-        }
-        else
-        {
-           addHistory(arg, numberArg, history);
-           execCmd(arg);
-        }
-    }
-    */
     
     return 0;
 }
